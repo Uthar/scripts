fbfcd3d7f80955e1ab8b00bab2484816f6a06718
kpg <>
2022-03-29 16:39:39 +0000

    dodane funkcje na wyciąganie najnowszych zmian dla każdego pliku
    FossilOrigin-Name: 2d3e733ed2f49f2fff98b52caf2351e6ae2a53679f9f5028f7d22fc814c8e34b

---
+++ sql/fossil-latest-changes.sql
@@ -0,0 +1,32 @@
+-- Kind of like file viewer on gitlab/github
+
+-- Show latest changes to:
+
+-- one file
+select f.name as name, e.comment, datetime(e.mtime) as d
+from filename f, event e, mlink m
+where f.name='flake.nix'
+and f.fnid=m.fnid
+and e.objid=m.mid
+and e.type='ci'
+order by d desc
+limit 1
+
+-- dir
+select f.name as name, e.comment, datetime(e.mtime) as d
+from filename f, event e, mlink m
+where f.name like 'modules/%'
+and f.fnid=m.fnid
+and e.objid=m.mid
+and e.type='ci'
+order by d desc
+limit 1
+
+-- all files
+select f.name as name, e.comment, datetime(e.mtime) as d
+from filename f, event e, mlink m
+where f.fnid=m.fnid
+and e.objid=m.mid
+and e.type='ci'
+group by name
+order by d desc
