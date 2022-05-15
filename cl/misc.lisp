484370b4b3ed463a1b28d5c16e8e54f2d17a341b
kpg <>
2022-05-15 22:14:29 +0000

    fix plural printing in human-readable-age
    FossilOrigin-Name: 74b4014990313712fcf7ebb1a8226f2081431ef79f0670f61c4905db1b0f5ae3

--- cl/misc.lisp
+++ cl/misc.lisp
@@ -1,9 +1,9 @@
 
 (defun human-readable-age (seconds)
   (cond
-    ((< seconds 1) "now")
+    ((< seconds 2) "now")
     ((< seconds 120) (format nil "~d seconds" seconds))
-    ((< seconds 3600) (format nil "~d minutes" (truncate seconds 60)))
+    ((< seconds 7200) (format nil "~d minutes" (truncate seconds 60)))
     ((< seconds (* 3600 36)) (format nil "~d hours" (truncate seconds 3600)))
     ((< seconds (* 3600 24 365)) (format nil "~d days" (truncate seconds (* 3600 24))))
     (t (format nil "~,1f years" (/ seconds 3600 24 365)))))
