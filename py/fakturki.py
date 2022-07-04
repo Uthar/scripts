591ce1107aee68547fd5e160e38fcd77b365906d
kpg <>
2022-07-04 07:27:43 +0000

    Dodałem skrypt na fakturki
    FossilOrigin-Name: 35a43540845443f76bc21deeb4b8061ac8306a3cc9ba99e50935538b5c95bf3f

---
+++ py/fakturki.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfReader, PdfWriter
+
+def make_filename(filename):
+    return filename.replace('/','-') + ".pdf"
+
+def extract_filename(page):
+        text = page.extractText()
+        lines = text.split()
+        filename = next(filter(lambda line: line.startswith("BL-"), lines))
+        return make_filename(filename)
+
+def split(filename):
+    reader = PdfReader(filename)
+    for n, page in enumerate(reader.pages):
+        writer = PdfWriter()
+        writer.add_page(page)
+        to = extract_filename(page)
+        with open(to, "wb") as fp:
+            writer.write(fp)
+    
