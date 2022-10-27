2621c72d59329bb099a0731bc76eedb2bf3fbf9f
root <root@noemail.net>
2022-10-27 21:54:17 +0000

    added promises
    FossilOrigin-Name: 8381007b33d871ef7694cd5ad1f18fa8201cd7c13c41fb7c3516baf58b7557ef

--- cl/pool.lisp
+++ cl/pool.lisp
@@ -1,38 +1,31 @@
-;; (defclass thread-pool ()
-;;   ((semaphore :initform nil)
-;;    (queue :initform nil)
-;;    (dispatcher :initform nil))
-;;   (:default-initargs
-;;    :size 1))
+(defpackage kaspi.pool
+  (:use :cl))
 
-;; (defun launch-job (job sem)
-;;   (sb-thread:wait-on-semaphore sem)
-;;   (sb-thread:make-thread
-;;    (lambda ()
-;;      (unwind-protect
-;;           (funcall job)
-;;        (sb-thread:signal-semaphore sem)))
-;;    :name "Thread pool worker"))
+(in-package kaspi.pool)
 
-;; (defun make-dispatcher (pool)
-;;   (sb-thread:make-thread
-;;    (lambda ()
-;;      (loop
-;;        (let ((job (sb-concurrency:receive-message (slot-value pool 'queue))))
-;;          (launch-job job (slot-value pool 'semaphore)))))
-;;    :name "Thread pool dispatcher"))
+;; Promises
 
-;; (defmethod initialize-instance :after ((pool thread-pool) &key size)
-;;   (setf (slot-value pool 'semaphore)  (sb-thread:make-semaphore :count size)
-;;         (slot-value pool 'queue)      (sb-concurrency:make-mailbox)
-;;         (slot-value pool 'dispatcher) (make-dispatcher pool)))
+(defclass promise ()
+  ((value :initform nil)
+   (resolvedp :initform nil)
+   (lock :initform (sb-thread:make-mutex))
+   (cv :initform (sb-thread:make-waitqueue))))
 
-;; (defmethod submit ((pool thread-pool) (job function))
-;;   (sb-concurrency:send-message (slot-value pool 'queue) job))
+(defmethod deref ((promise promise))
+  (sb-thread:with-mutex ((slot-value promise 'lock))
+    (or (slot-value promise 'resolvedp)
+        (sb-thread:condition-wait (slot-value promise 'cv)
+                                  (slot-value promise 'lock)))
+    (values (slot-value promise 'value)
+            (slot-value promise 'resolvedp))))
 
-;; (submit pool (lambda () (sleep 1) (print 42)))
-
-;; TODO dont launch new threads, run X threads with a recv loop instead
+(defmethod resolve ((promise promise) value)
+  (sb-thread:with-mutex ((slot-value promise 'lock))
+    (when (slot-value promise 'resolvedp)
+      (error "Promise is already resolved."))
+    (setf (slot-value promise 'value) value
+          (slot-value promise 'resolvedp) t)
+    (sb-thread:condition-notify (slot-value promise 'cv))))
 
 (require 'sb-concurrency)
 
@@ -59,15 +52,21 @@
 (defmethod shutdown ((pool thread-pool))
   (loop for thread across (slot-value pool 'threads)
         do (sb-thread:terminate-thread thread))
-  (setf (slot-value pool 'shutdownp) t))
+  (sb-ext:atomic-update (slot-value pool 'shutdownp) (constantly t)))
 
 (defmethod submit ((pool thread-pool) (job function))
   (when (slot-value pool 'shutdownp)
     (error "Pool has shut down."))
-  (sb-concurrency:send-message (slot-value pool 'queue) job))
+  (let ((promise (make-instance 'promise))
+        (job2 (lambda ()
+                (resolve promise (funcall job)))))
+    (sb-concurrency:send-message (slot-value pool 'queue) job2)
+    promise))
 
 (defparameter pool (make-instance 'thread-pool :size 5))
 
-(submit pool (lambda () (sleep 1) (print (slot-value pool 'queue))))
+(defparameter promise (submit pool (lambda () (sleep 5) (random 100))))
+
+(deref promise)
 
 (shutdown pool)
